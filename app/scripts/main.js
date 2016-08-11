bb9e82dd07e4b83532388fcdd680f6c8c2d943f7
chenhuiChang <j4demacia@gmail.com>
2016-08-11 18:02:55 +0800

    handle account closed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,36 @@
 var NO_PICTURE_URL = 'https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F',
     API_URL = 'https://api.twitch.tv/kraken/channels/',
+    JSONP_URL = 'https://api.twitch.tv/kraken/streams/',
     REAL_URL = 'https://www.twitch.tv/';
 var box = [];
 var onlineBox = [],
     offlineBox = [];
 var streamList = [
-    'https://api.twitch.tv/kraken/streams/freecodecamp?callback=?',
-    'https://api.twitch.tv/kraken/streams/storbeck?callback=?',
-    'https://api.twitch.tv/kraken/streams/esl_sc2?callback=?',
-    'https://api.twitch.tv/kraken/streams/asiagodtonegg3be0?callback=?',
-    'https://api.twitch.tv/kraken/streams/ogamingsc2?callback=?',
-    'https://api.twitch.tv/kraken/streams/p4wnyhof?callback=?',
-    'https://api.twitch.tv/kraken/streams/uzra?callback=?'
+    'freecodecamp',
+    'storbeck',
+    'esl_sc2',
+    'asiagodtonegg3be0',
+    'ogamingsc2',
+    'p4wnyhof',
+    'uzra',
+    'brunofin',
+    'giantwaffle',
+    'food'
 ];
 
 function ChannelData(logo, status, url) {
-    this.logo = logo || '';
-    this.name = url.replace(API_URL, '') || '';
-    this.url = url.replace(API_URL, REAL_URL) || '';
-    this.status = (!status) ? 'offline' : status.length + this.name.length > 60 ? status.substr(0, 50) + '...' : status;
-    this.shortStatus = (!status) ? 'offline' : status.length + this.name.length > 30 ? status.substr(0, 20) + '...' : status;
+    if (status === 'Account Closed') {
+        this.logo = logo || '';
+        this.name = url.replace('Channel \'', '').replace('\' is unavailable','') || '';
+        this.url = 'https://s.codepen.io/FreeCodeCamp/fullpage/undefined';
+        this.status = status;
+    } else {
+        this.logo = logo || '';
+        this.name = url.replace(API_URL, '') || '';
+        this.url = url.replace(API_URL, REAL_URL) || '';
+        this.status = (!status) ? 'offline' : status.length + this.name.length > 60 ? status.substr(0, 50) + '...' : status;
+        this.shortStatus = (!status) ? 'offline' : status.length + this.name.length > 30 ? status.substr(0, 20) + '...' : status;
+    }
 }
 
 function arrange(data) {
@@ -39,6 +50,10 @@ function arrange(data) {
             box.push(el);
             onlineBox.push(el);
         }
+    } else if (data.hasOwnProperty('error')) {
+        el = new ChannelData(NO_PICTURE_URL, 'Account Closed', data.message);
+        box.push(el);
+        offlineBox.push(el);
     }
 }
 function drawBox(arr) {
@@ -48,7 +63,7 @@ function drawBox(arr) {
 }
 function getItemString(ele) {
     var retStr = '<div class="channel-box';
-    if (ele.status!=='offline') {
+    if (ele.status!=='offline' && ele.status!=='Account Closed') {
         retStr += ' online">';
     } else retStr += ' offline">';
     retStr += '<img src="' + ele.logo + '" alt="" class="logo channel-box-item">';
@@ -69,10 +84,11 @@ function getData(url) {
 function makeList() {
     var AJAX = [];
     for (var i = 0; i < streamList.length; i++) {
-        AJAX.push(getData(streamList[i]));
+        AJAX.push(getData(JSONP_URL+streamList[i]+'?callback=?'));
     }
     $.when.apply($, AJAX).done(function () {
         for (var j = 0; j < arguments.length; j++) {
+            console.log(arguments[j]);
             if (arguments[j][1] !== 'success') {
                 console.log('connect error');
             } else {
