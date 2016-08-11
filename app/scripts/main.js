553150049f476730b31bbae3ef24e421e5523677
chenhuiChang <j4demacia@gmail.com>
2016-08-11 17:04:14 +0800

    add functions for getjson from twitch

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,8 @@
 var NO_PICTURE_URL = "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F",
     API_URL = 'https://api.twitch.tv/kraken/channels/',
     REAL_URL = 'https://www.twitch.tv/';
-var box = [{title:"Sprot1", content:"test" }];
-var onlineBox =[],
+var box = [];
+var onlineBox = [],
     offlineBox = [];
 var streamList = [
     'https://api.twitch.tv/kraken/streams/freecodecamp?callback=?',
@@ -11,61 +11,89 @@ var streamList = [
     'https://api.twitch.tv/kraken/streams/asiagodtonegg3be0?callback=?',
     'https://api.twitch.tv/kraken/streams/ogamingsc2?callback=?',
     'https://api.twitch.tv/kraken/streams/p4wnyhof?callback=?',
-    'https://api.twitch.tv/kraken/streams/uzra?callback=?',
-    // 'https://api.twitch.tv/kraken/streams/?callback=?',
+    'https://api.twitch.tv/kraken/streams/uzra?callback=?'
 ];
-var jsonReady = {};
 
 function ChannelData(logo, status, url) {
     this.logo = logo || "";
-
-    this.name = url.replace(API_URL,"") || "";
+    this.name = url.replace(API_URL, "") || "";
     this.url = url.replace(API_URL, REAL_URL) || "";
-    this.status = status || "offline";
-}
-function getAllStream(url, callback) {
-    $.getJSON(url, function (data) {
-        console.log(data);
-        callback && callback(data);
-    });
+    this.status = (!status) ? "offline" : status.length + this.name.length > 60 ? status.substr(0, 50) + "..." : status;
+    this.shortStatus = (!status) ? "offline" : status.length + this.name.length > 30 ? status.substr(0, 20) + "..." : status;
 }
 
-function temp(data) {
-    var el,ch;
+function arrange(data) {
+    var el, ch;
     if (data.hasOwnProperty("stream")) {
         if (data.stream === null) {
             if (data.hasOwnProperty("_links")) {
                 if (data._links.hasOwnProperty("channel")) {
-                    el = new ChannelData(NO_PICTURE_URL, "offline",data._links.channel); 
+                    el = new ChannelData(NO_PICTURE_URL, "offline", data._links.channel);
                     box.push(el);
-                    draw(el);
+                    offlineBox.push(el);
                 }
             }
         } else {
             ch = data.stream.channel;
             el = new ChannelData(ch.logo, ch.status, data._links.channel);
             box.push(el);
-            draw(el);
+            onlineBox.push(el);
         }
     }
 }
-function drawBox() {
-    box.forEach(function (element) {
+function drawBox(arr) {
+    arr.forEach(function (element) {
         $("#channel-list").append(getItemString(element));
     });
 }
-function draw(element) {
-    // console.log(getItemString(element));
-    $("#channel-list").append(getItemString(element));
-}
-function getItemString(ele){
-    var urlStr = "onclick=\"window.open(\'" + ele.url + "\', \'_blank\');\"";
-    return "<div "+ urlStr +"class=\"channel-box\" id=\"\"><h4>" + ele.name + "</h4><p>" + ele.status + "</p></div>";
+function getItemString(ele) {
+    var retStr = "<div class=\"channel-box";
+    if (ele.status!=="offline") {
+        retStr += " online\">";
+    } else retStr += " offline\">";
+    retStr += "<img src=\"" + ele.logo + "\" alt=\"\" class=\"logo channel-box-item\">";
+    retStr += "<div class=\"channel-box-item\"><a target=\"_blank\" href=\"" + ele.url + "\" class=\"title\">";
+    retStr += ele.name + "</a></div>"
+    retStr += "<div class=\"channel-box-item desc-long\"><p>" + ele.status + "</p></div>";
+    retStr += "<div class=\"channel-box-item desc-short\"><p>" + ele.shortStatus + "</p></div>";
+    return retStr;
 }
 
-function makeList(){
-    box.length = 0;
-    for(var i=0;i<streamList.length;i++){
-        getAllStream(streamList[i],temp);
-    }
+function clean() {
+    $("#channel-list").html("");
 }
+function getData(url) {
+    return $.getJSON(url);
+}
+
+function makeList() {
+    var AJAX = [];
+    for (var i = 0; i < streamList.length; i++) {
+        AJAX.push(getData(streamList[i]));
+    }
+    $.when.apply($, AJAX).done(function () {
+        for (var j = 0; j < arguments.length; j++) {
+            if (arguments[j][1] !== "success") {
+                console.log('connect error');
+            } else {
+                arrange(arguments[j][0]);
+            }
+        }
+        drawBox(onlineBox);
+        drawBox(offlineBox);
+    });
+}
+$(document).ready(makeList);
+$("#all").click(function(){
+    clean();
+    drawBox(onlineBox);
+    drawBox(offlineBox);
+});
+$("#on").click(function(){
+    clean();
+    drawBox(onlineBox);
+});
+$("#off").click(function(){
+    clean();
+    drawBox(offlineBox);
+});
