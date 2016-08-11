944fc980263aa2539106896ba0cb18a2a393d1f2
chenhuiChang <j4demacia@gmail.com>
2016-08-11 17:13:06 +0800

    add font family

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-var NO_PICTURE_URL = "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F",
+var NO_PICTURE_URL = 'https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F',
     API_URL = 'https://api.twitch.tv/kraken/channels/',
     REAL_URL = 'https://www.twitch.tv/';
 var box = [];
@@ -15,20 +15,20 @@ var streamList = [
 ];
 
 function ChannelData(logo, status, url) {
-    this.logo = logo || "";
-    this.name = url.replace(API_URL, "") || "";
-    this.url = url.replace(API_URL, REAL_URL) || "";
-    this.status = (!status) ? "offline" : status.length + this.name.length > 60 ? status.substr(0, 50) + "..." : status;
-    this.shortStatus = (!status) ? "offline" : status.length + this.name.length > 30 ? status.substr(0, 20) + "..." : status;
+    this.logo = logo || '';
+    this.name = url.replace(API_URL, '') || '';
+    this.url = url.replace(API_URL, REAL_URL) || '';
+    this.status = (!status) ? 'offline' : status.length + this.name.length > 60 ? status.substr(0, 50) + '...' : status;
+    this.shortStatus = (!status) ? 'offline' : status.length + this.name.length > 30 ? status.substr(0, 20) + '...' : status;
 }
 
 function arrange(data) {
     var el, ch;
-    if (data.hasOwnProperty("stream")) {
+    if (data.hasOwnProperty('stream')) {
         if (data.stream === null) {
-            if (data.hasOwnProperty("_links")) {
-                if (data._links.hasOwnProperty("channel")) {
-                    el = new ChannelData(NO_PICTURE_URL, "offline", data._links.channel);
+            if (data.hasOwnProperty('_links')) {
+                if (data._links.hasOwnProperty('channel')) {
+                    el = new ChannelData(NO_PICTURE_URL, 'offline', data._links.channel);
                     box.push(el);
                     offlineBox.push(el);
                 }
@@ -43,24 +43,24 @@ function arrange(data) {
 }
 function drawBox(arr) {
     arr.forEach(function (element) {
-        $("#channel-list").append(getItemString(element));
+        $('#channel-list').append(getItemString(element));
     });
 }
 function getItemString(ele) {
-    var retStr = "<div class=\"channel-box";
-    if (ele.status!=="offline") {
-        retStr += " online\">";
-    } else retStr += " offline\">";
-    retStr += "<img src=\"" + ele.logo + "\" alt=\"\" class=\"logo channel-box-item\">";
-    retStr += "<div class=\"channel-box-item\"><a target=\"_blank\" href=\"" + ele.url + "\" class=\"title\">";
-    retStr += ele.name + "</a></div>"
-    retStr += "<div class=\"channel-box-item desc-long\"><p>" + ele.status + "</p></div>";
-    retStr += "<div class=\"channel-box-item desc-short\"><p>" + ele.shortStatus + "</p></div>";
+    var retStr = '<div class="channel-box';
+    if (ele.status!=='offline') {
+        retStr += ' online">';
+    } else retStr += ' offline">';
+    retStr += '<img src="' + ele.logo + '" alt="" class="logo channel-box-item">';
+    retStr += '<div class="channel-box-item"><a target="_blank" href="' + ele.url + '" class="title">';
+    retStr += ele.name + '</a></div>'
+    retStr += '<div class="channel-box-item desc-long"><p>' + ele.status + '</p></div>';
+    retStr += '<div class="channel-box-item desc-short"><p>' + ele.shortStatus + '</p></div>';
     return retStr;
 }
 
 function clean() {
-    $("#channel-list").html("");
+    $('#channel-list').html('');
 }
 function getData(url) {
     return $.getJSON(url);
@@ -73,7 +73,7 @@ function makeList() {
     }
     $.when.apply($, AJAX).done(function () {
         for (var j = 0; j < arguments.length; j++) {
-            if (arguments[j][1] !== "success") {
+            if (arguments[j][1] !== 'success') {
                 console.log('connect error');
             } else {
                 arrange(arguments[j][0]);
@@ -84,16 +84,16 @@ function makeList() {
     });
 }
 $(document).ready(makeList);
-$("#all").click(function(){
+$('#all').click(function(){
     clean();
     drawBox(onlineBox);
     drawBox(offlineBox);
 });
-$("#on").click(function(){
+$('#on').click(function(){
     clean();
     drawBox(onlineBox);
 });
-$("#off").click(function(){
+$('#off').click(function(){
     clean();
     drawBox(offlineBox);
 });
